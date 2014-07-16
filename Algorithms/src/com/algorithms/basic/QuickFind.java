4775e9becb809a354aad14397549a82795f32fb8
amitvc <achavan1@gmail.com>
2014-07-16 11:06:47 -0400

    Added initialization code for QuickFind algorithm

--- Algorithms/src/com/algorithms/basic/QuickFind.java
+++ Algorithms/src/com/algorithms/basic/QuickFind.java
@@ -2,9 +2,26 @@ package com.algorithms.basic;
 
 public class QuickFind {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	int elements[];
+	
+	/**
+	 * initialize the array and also elements.
+	 * Initially all elements will be initialized to the their own
+	 * index value.
+	 * @param SIZE
+	 */
+	public QuickFind(int SIZE) {
+		elements = new int[SIZE];
+		for(int i=0; i<SIZE; i++) {
+			elements[i] = i;
+		}
 	}
-
+	
+	/**
+	 * Entry point to the program
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
 }
